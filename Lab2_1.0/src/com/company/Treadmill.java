fc10abc65a07cb3dd92165087d4b5452beae814d
PMV-cute <79105726+PMV-cute@users.noreply.github.com>
2021-09-27 17:09:25 +0300

    Delete Lab2_1.0 directory

--- Lab2_1.0/src/com/company/Treadmill.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Treadmill {
-    private int dist;
-
-    public Treadmill(int dist) { this.dist = dist; }
-    public int getRange() { return dist; }
-    public void setRange(int dist) { this.dist = dist; }
-}
