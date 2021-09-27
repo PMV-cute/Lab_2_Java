5b558ad17fa6b297d4cbcbf3eeadd23c778835c0
PMV-cute <79105726+PMV-cute@users.noreply.github.com>
2021-09-27 17:11:08 +0300

    Delete Lab2_1.0 directory

--- Lab2_1.0/src/com/company/Team.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public interface Team {
-    int getI();
-    void RunJump(trials[] trials1);
-
-}
--- Lab2_1.0/src/com/company/Treadmill.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Treadmill implements trials {
-    private int dist;
-
-    public Treadmill(int dist) { this.dist = dist; }
-    public int get() { return dist; }
-    public void set(int dist) { this.dist = dist; }
-}
